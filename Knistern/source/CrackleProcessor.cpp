9e97f37fea6762a3f24380a4c6f83cc9f5754ad9
Eric <ericlambrecht.hh@gmail.com>
2014-01-20 15:39:07 +0100

    Some changes

--- Knistern/source/CrackleProcessor.cpp
+++ Knistern/source/CrackleProcessor.cpp
@@ -1,13 +1,21 @@
 #include "CrackleProcessor.h"
 
 CrackleProcessor::CrackleProcessor()
-	: sampleRate(0), delay(0), minDelay(0)
+	: sampleRate(0), crackleDepth(1)
 {
 }
 
 void CrackleProcessor::initialize(float sampleRate) {
 	this->sampleRate = sampleRate;
-	cg.initialize({ 0, 0.1, 0.5, 1, 0.9, 0.7, 0.4 }, 7);
+	float sample[7];
+	sample[0] = 0;
+	sample[1] = 0.1;
+	sample[2] = 0.5;
+	sample[3] = 1;
+	sample[4] = 0.9;
+	sample[5] = 0.7;
+	sample[6] = 0.4; 
+	cg.initialize(sample, 7);
 }
 
 void CrackleProcessor::setCrackleAmount(float amount) {
